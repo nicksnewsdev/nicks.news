2e5abda134547bae708346c604264c7da17399b7
NicksNews <nicksnewscat@gmail.com>
2026-04-18 13:05:26 +0200

    nicks.news v2

---
+++ src/pages/experience.js
@@ -0,0 +1,8 @@
+export const EXPERIENCE = () => {
+    return `
+        <div class='experience'>
+            <h1>Experience</h1>
+            <p>No content</p>
+        </div>
+    `;
+};
---
+++ src/pages/projects.js
@@ -0,0 +1,8 @@
+export const PROJECTS = () => {
+    return `
+        <div class='projects'>
+            <h1>Projects</h1>
+            <p>No content</p>
+        </div>
+    `;
+};
